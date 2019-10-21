20fb93b36b0f9341e1f30a861be7f043895b2086
Jeff <jeff@almond.group>
2019-10-21 15:42:42 +0700

    [Update] Change API link

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   
   useEffect(() => {
     async function fetchData() {
-      const response = await axios.get(`jjayeff/api`)
+      const response = await axios.get(`jjayeff.com/api`)
       setData(response.data)
     }
 
